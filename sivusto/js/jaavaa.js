b37e95636e5e435d4890c1ec88241f8cce154136
ShivadeT0T <maxkolomainen@hotmail.com>
2022-12-02 00:42:19 +0200

    Changed the way map looks.

--- sivusto/js/jaavaa.js
+++ sivusto/js/jaavaa.js
@@ -2,9 +2,9 @@
 /* 1. show map using Leaflet library. (L comes from the Leaflet library) */
 
 const map = L.map('map', { tap: false });
-L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-  maxZoom: 20,
-  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+  attribution: 'Stamen',
+  maxZoom: 10,
 }).addTo(map);
 map.setView([60, 24], 7);
 
