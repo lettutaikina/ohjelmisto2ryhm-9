e1e853fb5d6bce7472c831cb268ad79302f2f08d
Possu001 <juusokor@gmail.com>
2022-12-01 09:43:13 +0200

    backend avattu

---
+++ bakkend/game.py
@@ -0,0 +1,78 @@
+import string, random
+from airport import Airport
+import config
+
+class Game:
+
+    def __init__(self, id, loc, consumption, player=None):
+        self.status = {}
+        self.location = []
+        self.goals = []
+
+        if id==0:
+            # new game
+            # Create new game id
+            letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+            self.status = {
+                "id" : ''.join(random.choice(letters) for i in range(20)),
+                "name" : player,
+                "co2" : {
+                    "consumed" : config.co2_initial,
+                    "budget" : config.co2_budget
+                },
+                "previous_location" : ""
+            }
+
+
+            #self.id = ''.join(random.choice(letters) for i in range(20))
+            #self.footprint = config.initial_footprint
+            self.location.append(Airport(loc, True))
+            #self.player = player
+            # Insert new game into DB
+            sql = "INSERT INTO Game VALUES ('" + self.status["id"] + "', " + str(self.status["co2"]["consumed"])
+            sql += ", " + str(self.status["co2"]["budget"]) + ", '" + loc + "', '" + self.status["name"] + "')"
+            print(sql)
+            cur = config.conn.cursor()
+            cur.execute(sql)
+            #config.conn.commit()
+
+        else:
+            #update consumption and budget
+            sql2 = "UPDATE Game SET co2_consumed = co2_consumed + " + consumption + ", co2_budget = co2_budget - " + consumption + " WHERE id='" + id + "'"
+            print(sql2)
+            cur2 = config.conn.cursor()
+            cur2.execute(sql2)
+            # find game from DB
+            sql = "SELECT id, co2_consumed, co2_budget, location, screen_name FROM Game WHERE id='" + id + "'"
+            print(sql)
+            cur = config.conn.cursor()
+            cur.execute(sql)
+            res = cur.fetchall()
+            if len(res) == 1:
+                # game found
+                self.status = {
+                    "id": res[0][0],
+                    "name": res[0][4],
+                    "co2": {
+                        "consumed": res[0][1],
+                        "budget": res[0][2]
+                    },
+                    "previous_location" : res[0][3]
+                }
+                # old location in DB currently not used
+                apt = Airport(loc, True)
+                self.location.append(apt)
+                self.set_location(apt)
+
+            else:
+                print("************** PELIÄ EI LÖYDY! ***************")
+
+    def set_location(self, sijainti):
+        #self.location = sijainti
+        sql = "UPDATE Game SET location='" + sijainti.ident + "' WHERE id='" + self.status["id"] + "'"
+        print(sql)
+        cur = config.conn.cursor()
+        cur.execute(sql)
+        #config.conn.commit()
+        #self.loc = sijainti.ident
---
+++ bakkend/weather.py
@@ -0,0 +1,27 @@
+import requests
+import config
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class Weather:
+
+    def kelvin_to_celsius(self, kelvin):
+        return int (kelvin - 273.15)
+
+    def __init__(self, sijainti, game):
+        apikey = os.environ.get('API_KEY')
+
+        request = "https://api.openweathermap.org/data/2.5/weather?lat=" + \
+                 str(sijainti.latitude) + "&lon=" + str(sijainti.longitude) + "&appid=" + apikey
+        vastaus = requests.get(request).json()
+        self.main = vastaus["weather"][0]["main"]
+        self.description = vastaus["weather"][0]["description"]
+        self.icon = "https://openweathermap.org/img/wn/" + vastaus["weather"][0]["icon"] + ".png"
+        self.temp = self.kelvin_to_celsius(vastaus["main"]["temp"])
+        self.humidity = vastaus["main"]["humidity"]
+        self.wind = {
+            "speed": vastaus["wind"]["speed"],
+            "deg": vastaus["wind"]["deg"]
+        }
