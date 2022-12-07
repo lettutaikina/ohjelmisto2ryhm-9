0dc13bc8d2c5a62a5fcf36ebcebaccd0e07631d7
Possu001 <juusokor@gmail.com>
2022-12-07 13:32:03 +0200

    turhaa dokumentointia vähäsen

--- bakkend/config.py
+++ bakkend/config.py
@@ -1,15 +1,17 @@
 # Game parameters
+#max distance where you can fly / draw airports
 max_distance = 1000
 max_lat_dist = max_distance/50
 max_lon_dist = max_distance/50
 
+#budget etc. for fuel
 co2_initial = 0
 co2_budget = 10000
 co2_per_flight = 50
-co2_if_rain = 150
+#co2_if_rain = 150
 co2_per_km = 1
 
-
+#ei käytetä oikeesti , tulee javascriptist
 default_starting_point = "EFRO"
 default_name = "DDK"
 
