819c5f09b0b1df759f528618fb8cc62eb2bd2fe3
Possu001 <juusokor@gmail.com>
2022-12-09 13:56:05 +0200

    -distance -budget

--- bakkend/config.py
+++ bakkend/config.py
@@ -1,12 +1,12 @@
 # Game parameters
 #max distance where you can fly / draw airports
-max_distance = 1000
+max_distance = 500
 max_lat_dist = max_distance/50
 max_lon_dist = max_distance/50
 
 #budget etc. for fuel
 co2_initial = 0
-co2_budget = 10000
+co2_budget = 7500
 co2_per_flight = 50
 co2_if_rain = 500
 co2_per_km = 1
